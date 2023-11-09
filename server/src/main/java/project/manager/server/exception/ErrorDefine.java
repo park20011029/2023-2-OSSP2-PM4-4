bc03152083af464b3256f186f469f95612be0a00
park20011029 <rokmck156@gmail.com>
2023-11-09 11:06:43 +0900

    Feat: Add Resume CRUD error

--- server/src/main/java/project/manager/server/exception/ErrorDefine.java
+++ server/src/main/java/project/manager/server/exception/ErrorDefine.java
@@ -10,13 +10,15 @@ public enum ErrorDefine {
     // NOT_FOUND: 404
     USER_NOT_FOUND("4040", HttpStatus.NOT_FOUND, "Not Found: User Not Found"),
     USER_WITHDRAWAL("4041", HttpStatus.NOT_FOUND, "Not Found: User withdraw"),
-
+    RESUME_NOT_FOUND("4042", HttpStatus.NOT_FOUND, "NOT Found: Resume Not Found"),
     // CONFLICT: 409
     EMAIL_EXIST(
             "4090", HttpStatus.CONFLICT, "Conflict: An account with this email already exists."),
     USER_EXIST("4091", HttpStatus.CONFLICT, "Conflict: An account with this user already exists."),
     NICKNAME_EXIST(
             "4092", HttpStatus.CONFLICT, "Conflict: An account with this nickname already exists."),
+    RESUME_EXIST("4093",HttpStatus.CONFLICT, "Conflict: An account with this Resume already exits"),
+
 
     // GONE: 410
     USER_DELETE("4010", HttpStatus.GONE, "GONE: User delete data"),
