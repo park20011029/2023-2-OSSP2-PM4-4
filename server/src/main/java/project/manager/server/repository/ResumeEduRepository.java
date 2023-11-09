3a49da6b24517f027780e9060735a17e83c1d209
park20011029 <rokmck156@gmail.com>
2023-11-09 11:16:15 +0900

    Feat: Add resumeaedu repository with basic structure

---
+++ server/src/main/java/project/manager/server/repository/ResumeEduRepository.java
@@ -0,0 +1,8 @@
+package project.manager.server.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import project.manager.server.domain.ResumeEdu;
+
+public interface ResumeEduRepository extends JpaRepository<ResumeEdu, Long> {
+
+}
