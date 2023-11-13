55acb17730c6344a5e5c049b896ff21251c07968
Sangmin Han <gkstkdals07@naver.com>
2023-11-13 23:04:53 +0900

    Feat: Add si dto and gu dto

---
+++ server/src/main/java/project/manager/server/dto/reponse/region/SiDto.java
@@ -0,0 +1,11 @@
+package project.manager.server.dto.reponse.region;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class SiDto {
+    private String si;
+    private Long siId;
+}
