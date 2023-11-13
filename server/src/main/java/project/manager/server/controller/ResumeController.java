23bdf5e82cf96bbaecd22452b110456000dddf88
Sangmin Han <gkstkdals07@naver.com>
2023-11-13 23:16:25 +0900

    Refactor: Refactor resume controller

--- server/src/main/java/project/manager/server/controller/ResumeController.java
+++ server/src/main/java/project/manager/server/controller/ResumeController.java
@@ -1,29 +1,27 @@
 package project.manager.server.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import project.manager.server.domain.User;
 import project.manager.server.dto.ResponseDto;
-import project.manager.server.dto.reponse.ResumeDto;
-import project.manager.server.dto.request.ResumeRequestDto;
+import project.manager.server.dto.reponse.resume.ResumeDto;
+import project.manager.server.dto.request.resume.ResumeRequestDto;
+import project.manager.server.service.resume.ResumeService;
 
-    @RestController
-    @RequestMapping("/resume")
-    @RequiredArgsConstructor
-    public class ResumeController {
-        private  final project.manager.server.service.ResumeService resumeService;
 
-        @PostMapping("/signup")
-        public ResponseDto<ResumeDto> resumeSignUp(@RequestBody ResumeRequestDto resumeRequestDto){
-            return new ResponseDto<ResumeDto>(resumeService.createResume(resumeRequestDto));
-        }
+@RestController
+@RequestMapping("/resume")
+@RequiredArgsConstructor
+public class ResumeController {
+    private final ResumeService resumeService;
 
-        @PutMapping("/{userId}")
-        public ResponseDto<ResumeDto> updateResume(
-                @PathVariable User user, @RequestBody ResumeRequestDto resumeRequestDto
-        ){
-            return new ResponseDto<ResumeDto>(resumeService.updateResume(user, resumeRequestDto));
-        }
-
+    @PostMapping("/{userId}")
+    public ResponseDto<Long> creatResume(@PathVariable Long userId, @Valid @RequestBody ResumeRequestDto resumeRequestDto) {
+        return new ResponseDto<Long>(resumeService.createResume(userId, resumeRequestDto));
     }
 
+    @GetMapping("/{userId}")
+    public ResponseDto<ResumeDto> readResume(@PathVariable Long userId) {
+        return new ResponseDto<ResumeDto>(resumeService.readResume(userId));
+    }
+}
