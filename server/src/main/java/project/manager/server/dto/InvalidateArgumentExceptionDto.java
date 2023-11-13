5d291753d3b07e196d121ea7c5ef31719c71a03d
Sangmin Han <gkstkdals07@naver.com>
2023-11-13 22:35:04 +0900

    Feat: Add exception handler for MethodArgumentNotValidException

---
+++ server/src/main/java/project/manager/server/dto/InvalidateArgumentExceptionDto.java
@@ -0,0 +1,22 @@
+package project.manager.server.dto;
+
+import lombok.Getter;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.validation.FieldError;
+import project.manager.server.exception.ErrorDefine;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Getter
+public class InvalidateArgumentExceptionDto extends ExceptionDto {
+    private final Map<String, String> errorFields;
+
+    public InvalidateArgumentExceptionDto(MethodArgumentNotValidException invalidException) {
+        super(ErrorDefine.INVALID_ARGUMENT);
+
+        this.errorFields = invalidException.getBindingResult().getFieldErrors().stream()
+                .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+    }
+
+}
--- server/src/main/java/project/manager/server/exception/GlobalRestExceptionHandler.java
+++ server/src/main/java/project/manager/server/exception/GlobalRestExceptionHandler.java
@@ -1,6 +1,7 @@
 package project.manager.server.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import project.manager.server.dto.ResponseDto;
@@ -12,8 +13,15 @@ public class GlobalRestExceptionHandler {
         return ResponseDto.toResponseEntity(e);
     }
 
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    public ResponseEntity<?> handleInvalidArgumentException(MethodArgumentNotValidException e) {
+        return ResponseDto.toResponseEntity(e);
+    }
+
     @ExceptionHandler(value = {Exception.class})
     public ResponseEntity<?> handleException(Exception e) {
         return ResponseDto.toResponseEntity(e);
     }
+
+
 }
