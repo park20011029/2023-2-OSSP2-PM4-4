b1253ac0d3b3dfc8a9b1f278bbc4fa7e0e254226
Sangmin Han <gkstkdals07@naver.com>
2023-11-14 20:37:55 +0900

    Refactor: Handle nullpoint exception

--- server/src/main/java/project/manager/server/dto/reponse/UserDto.java
+++ server/src/main/java/project/manager/server/dto/reponse/UserDto.java
@@ -1,16 +1,20 @@
 package project.manager.server.dto.reponse;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import project.manager.server.domain.User;
+import project.manager.server.json.NullToNullStringSerializer;
 
 @Getter
 @NoArgsConstructor
 public class UserDto {
 
-    private Long userId;
+    @JsonSerialize(using = NullToNullStringSerializer.class)
     private Long resumeId;
+
+    private Long userId;
     private String name;
     private String email;
     private String nickName;
@@ -19,7 +23,7 @@ public class UserDto {
 
     @Builder
     public UserDto(User user) {
-        this.resumeId = user.getResume().getId();
+        this.resumeId = (user.getResume() != null) ? user.getResume().getId() : null;
         this.userId = user.getId();
         this.name = user.getName();
         this.email = user.getEmail();
