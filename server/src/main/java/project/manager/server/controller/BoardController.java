1c4a8b5b6ac0c96628ef2cd380d2c579a6ea9c3b
park20011029 <rokmck156@gmail.com>
2023-11-17 19:39:35 +0900

    ﻿# 제목은 최대 50글자까지 아래에 작성: ex) Feat: Add Key mapping
    Feat: Add board controller with basic structure

---
+++ server/src/main/java/project/manager/server/controller/BoardController.java
@@ -0,0 +1,16 @@
+/*
+package project.manager.server.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import project.manager.server.service.gongmo.GongMoService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/board")
+public class BoardController {
+    private final GongMoService gongMoService;
+    private final BoardService boardService;
+}
+*/
