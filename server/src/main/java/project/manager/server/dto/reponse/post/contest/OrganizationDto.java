b04803c9d18c7dd3f3a82870b1927811845b96af
Sangmin Han <gkstkdals07@naver.com>
2023-11-22 16:10:56 +0900

    Feat: Add post type dto

---
+++ server/src/main/java/project/manager/server/dto/reponse/post/contest/OrganizationDto.java
@@ -0,0 +1,20 @@
+package project.manager.server.dto.reponse.post.contest;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import project.manager.server.domain.post.contest.Organization;
+
+@Getter
+@NoArgsConstructor
+public class OrganizationDto {
+
+    private Long id;
+    private String organization;
+
+    @Builder
+    public OrganizationDto(Organization organization) {
+        this.id = organization.getId();
+        this.organization = organization.getOrganization();
+    }
+}
