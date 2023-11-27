fdc4cf6b6ec75bd38fb8dddc6c32757a0671c9f5
Sangmin Han <gkstkdals07@naver.com>
2023-11-27 14:23:07 +0900

    Comment: Add comment in controller for FE developer

--- server/src/main/java/project/manager/server/controller/PostTypeController.java
+++ server/src/main/java/project/manager/server/controller/PostTypeController.java
@@ -1,5 +1,7 @@
 package project.manager.server.controller;
 
+import jakarta.validation.Valid;
+
 import java.util.Map;
 
 import lombok.RequiredArgsConstructor;
@@ -21,53 +23,73 @@ public class PostTypeController {
     private final ScaleService scaleService;
     private final TargetService targetService;
 
+    //공모전 카테고리 중 수상혜택 생성
     @PostMapping("/benefit")
-    ResponseDto<Boolean> createBenefit(@RequestBody PostTypeRequestDto.BenefitDto benefitDto) {
+    ResponseDto<Boolean> createBenefit(@Valid @RequestBody PostTypeRequestDto.BenefitDto benefitDto) {
+
         return new ResponseDto<>(benefitService.createBenefit(benefitDto));
     }
 
+    //공모전 카테고리 중 공모분야 생성
     @PostMapping("/category")
-    ResponseDto<Boolean> createCategory(@RequestBody PostTypeRequestDto.CategoryDto categoryDto) {
+    ResponseDto<Boolean> createCategory(@Valid @RequestBody PostTypeRequestDto.CategoryDto categoryDto) {
+
         return new ResponseDto<>(categoryService.createCategory(categoryDto));
     }
 
+    //공모전 카테고리 중 주최기관 생성
     @PostMapping("/organization")
-    ResponseDto<Boolean> createOrganization(@RequestBody PostTypeRequestDto.OrganizationDto organizationDto) {
+    ResponseDto<Boolean> createOrganization(@Valid @RequestBody PostTypeRequestDto.OrganizationDto organizationDto) {
+
         return new ResponseDto<>(organizationService.createOrganization(organizationDto));
     }
 
+    //공모전 카테고리 중 시상규모 생성
     @PostMapping("/scale")
-    ResponseDto<Boolean> createScale(@RequestBody PostTypeRequestDto.ScaleDto scaleDto) {
+    ResponseDto<Boolean> createScale(@Valid @RequestBody PostTypeRequestDto.ScaleDto scaleDto) {
+
         return new ResponseDto<>(scaleService.createScale(scaleDto));
     }
 
+    //공모전 카테고리 중 응모대상 생성
     @PostMapping("/target")
-    ResponseDto<Boolean> createTarget(@RequestBody PostTypeRequestDto.TargetDto targetDto) {
+    ResponseDto<Boolean> createTarget(@Valid @RequestBody PostTypeRequestDto.TargetDto targetDto) {
+
         return new ResponseDto<>(targetService.createTarget(targetDto));
     }
 
+    //수상혜택 목록 읽어오기
     @GetMapping("/benefit")
     ResponseDto<Map<String, Object>> readBenefitList() {
+
         return new ResponseDto<>(benefitService.readBenefitList());
     }
 
+    //공모분야 목록 읽어오기
     @GetMapping("/category")
     ResponseDto<Map<String, Object>> readCategoryList() {
+
         return new ResponseDto<>(categoryService.readCategoryList());
     }
 
+    //주최기관 목록 읽어오기
     @GetMapping("/organization")
     ResponseDto<Map<String, Object>> readOrganzationList() {
+
         return new ResponseDto<>(organizationService.readOrganizationList());
     }
 
+    //시상규모 목록 읽어오기
     @GetMapping("/scale")
     ResponseDto<Map<String, Object>> readScaleList() {
+
         return new ResponseDto<>(scaleService.readScaleList());
     }
 
+    //응모대상 목록 읽어오기
     @GetMapping("/target")
     ResponseDto<Map<String, Object>> readTargetList() {
+
         return new ResponseDto<>(targetService.readTargetList());
     }
 
--- server/src/main/java/project/manager/server/controller/RegionController.java
+++ server/src/main/java/project/manager/server/controller/RegionController.java
@@ -18,14 +18,17 @@ import project.manager.server.service.RegionService;
 @RequestMapping("/region")
 @RequiredArgsConstructor
 public class RegionController {
+
     private final RegionService regionService;
 
+    //시 목록 읽어오기
     @GetMapping("/si")
     public ResponseDto<List<SiDto>> showSiList() {
 
         return new ResponseDto<List<SiDto>>(regionService.readSiList());
     }
 
+    //구 목록 읽어오기
     @GetMapping("/gu/{siId}")
     public ResponseDto<List<GuDto>> showGuList(@PathVariable Long siId) {
 
