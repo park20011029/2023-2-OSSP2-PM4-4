e05cd3de047cac2d74b57f9848c8435c50fc2416
Sangmin Han <gkstkdals07@naver.com>
2023-11-27 21:42:26 +0900

    Feat: Add request dto for building post

--- server/src/main/java/project/manager/server/dto/request/post/building/BuildingPostUpdateDto.java
+++ server/src/main/java/project/manager/server/dto/request/post/building/BuildingPostUpdateDto.java
@@ -1,9 +1,6 @@
 package project.manager.server.dto.request.post.building;
 
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-
-import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,14 +9,9 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class BuildingPostRequestDto {
-
-    @NotNull
-    private Long userId;
+public class BuildingPostUpdateDto {
     @NotNull
     private String title;
     @NotNull
     private String content;
-    @NotEmpty
-    private List<PartRequestDto> partList;
 }
