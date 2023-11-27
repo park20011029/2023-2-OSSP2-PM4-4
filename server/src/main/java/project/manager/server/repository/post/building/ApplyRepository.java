8cb0e1720addce19df2a8cd2dbd56967b58984a8
Sangmin Han <gkstkdals07@naver.com>
2023-11-27 21:50:08 +0900

    Feat: Add repository for building post

--- server/src/main/java/project/manager/server/repository/post/building/ApplyRepository.java
+++ server/src/main/java/project/manager/server/repository/post/building/ApplyRepository.java
@@ -1,10 +1,20 @@
 package project.manager.server.repository.post.building;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import project.manager.server.domain.post.building.Apply;
 
+import java.util.Optional;
+
 @Repository
 public interface ApplyRepository extends JpaRepository<Apply, Long> {
+
+    Optional<Apply> findById(Long applyId);
+
+    @Query("SELECT a FROM Apply a JOIN FETCH a.applicant ap WHERE a.id = :applyId")
+    Optional<Apply> findByIdWithApplicant(@Param("applyId") Long applyId);
+
 }
--- server/src/main/java/project/manager/server/repository/post/building/BuildingPostRepository.java
+++ server/src/main/java/project/manager/server/repository/post/building/BuildingPostRepository.java
@@ -2,12 +2,23 @@ package project.manager.server.repository.post.building;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import project.manager.server.domain.post.building.BuildingPost;
+import project.manager.server.domain.post.contest.ContestPost;
 
 @Repository
 public interface BuildingPostRepository extends JpaRepository<BuildingPost, Long> {
     Optional<BuildingPost> findById(Long buildingPostId);
+
+    @Query("SELECT bp FROM BuildingPost bp JOIN FETCH bp.writer WHERE bp.id = :buildingPostId AND bp.isDelete = false")
+    Optional<BuildingPost> findByIdAndIsDeleteFalse(Long buildingPostId);
+
+    @Query("SELECT b FROM BuildingPost b JOIN FETCH b.writer WHERE b.contestPost = :contestPost AND b.isDelete = false")
+    Page<BuildingPost> findByContestPostWithUser(ContestPost contestPost, Pageable pageInfo);
+
 }
