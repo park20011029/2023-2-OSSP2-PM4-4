24202caf97e05da18649085823b62d96e59b560f
Sangmin Han <gkstkdals07@naver.com>
2023-12-03 01:33:05 +0900

    Refactor: remove useless feature and annotation

--- server/src/main/java/project/manager/server/service/resume/ProjectService.java
+++ server/src/main/java/project/manager/server/service/resume/ProjectService.java
@@ -1,7 +1,5 @@
 package project.manager.server.service.resume;
 
-import java.util.List;
-import java.util.stream.Collectors;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,52 +8,38 @@ import org.springframework.transaction.annotation.Transactional;
 
 import project.manager.server.domain.resume.Project;
 import project.manager.server.domain.resume.Resume;
-import project.manager.server.dto.reponse.resume.ProjectDto;
-import project.manager.server.dto.request.resume.ProjectRequestDto;
+import project.manager.server.dto.request.resume.create.ProjectRequestDto;
 import project.manager.server.exception.ApiException;
 import project.manager.server.exception.ErrorDefine;
 import project.manager.server.repository.resume.ProjectRepository;
+import project.manager.server.repository.resume.ResumeRepository;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class ProjectService {
     private final ProjectRepository projectRepository;
+    private final ResumeRepository resumeRepository;
 
-    public Project createProject(ProjectRequestDto projectRequestDto, Resume resume) {
+    public Long addProject(Long resumeId, ProjectRequestDto projectRequestDto) {
+        Resume resume = resumeRepository.findById(resumeId)
+                .orElseThrow(() -> new ApiException(ErrorDefine.RESUME_NOT_FOUND));
 
         Project newProject = Project.builder()
-                        .resume(resume)
-                        .projectName(projectRequestDto.getProjectName())
-                        .description(projectRequestDto.getDescription())
-                        .gitUrl(projectRequestDto.getGitUrl())
-                        .build();
+                .resume(resume)
+                .projectName(projectRequestDto.getProjectName())
+                .gitUrl(projectRequestDto.getGitUrl())
+                .description(projectRequestDto.getDescription())
+                .build();
+        projectRepository.save(newProject);
 
-        return projectRepository.save(newProject);
+        return newProject.getId();
     }
 
-    public List<ProjectDto> readProject(List<Project> projects) {
-        if (projects == null) return null;
-
-        return projects.stream()
-                .map(project -> ProjectDto.builder().project(project).build())
-                .collect(Collectors.toList());
-    }
-
-    public void updateProject(ProjectRequestDto projectRequestDto) {
-        Project project = projectRepository.findById(projectRequestDto.getId())
-                        .orElseThrow(() -> new ApiException(ErrorDefine.ENTITY_NOT_FOUND));
-
-        project.updateProject(
-                projectRequestDto.getProjectName(),
-                projectRequestDto.getGitUrl(),
-                projectRequestDto.getDescription());
-    }
 
     public boolean deleteProject(Long projectId) {
         Project project = projectRepository.findById(projectId)
                         .orElseThrow(() -> new ApiException(ErrorDefine.ENTITY_NOT_FOUND));
-
         projectRepository.delete(project);
 
         return true;
