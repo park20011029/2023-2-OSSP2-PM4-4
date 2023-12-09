8fd25240089797875e72b433369c5a56332b481c
Sangmin Han <gkstkdals07@naver.com>
2023-12-10 00:14:27 +0900

    Feat: Add security

---
+++ server/src/main/java/project/manager/server/security/CustomUserDetailService.java
@@ -0,0 +1,27 @@
+package project.manager.server.security;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import project.manager.server.repository.UserRepository;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserRepository.UserLoginForm user = userRepository
+                .findUserForAuthentication(Long.valueOf(username))
+                .orElseThrow(() -> new UsernameNotFoundException("ACCESS_DENIED_ERROR"));
+
+        return CustomUserDetail.create(user);
+    }
+}
