2863f25ee1835b87da513b0b0a53b1c0ad041bc6
JunSeop00 <wonjs00@naver.com>
2023-12-18 19:53:45 +0900

    Design: Modify css for ad component

--- client/src/main/Ad.js
+++ client/src/main/Ad.js
@@ -1,7 +1,7 @@
 function Ad(url) {
   return (
     <div className="flex items-center justify-center border-2 border-[#000000]
-                    mx-auto my-[50px] object-none">
+                    mx-auto mb-[50px] object-none">
       <img src='image.png'></img>
     </div>
   );
