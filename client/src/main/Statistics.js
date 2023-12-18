3bd6d94cf853639555191e71ae2cc57181010393
JunSeop00 <wonjs00@naver.com>
2023-12-18 19:55:51 +0900

    Design: Modify css for statistics component

--- client/src/main/Statistics.js
+++ client/src/main/Statistics.js
@@ -9,24 +9,23 @@ function Statistics() {
 
   const [currentStatIndex, setCurrentStatIndex] = useState(0);
 
-  /*
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentStatIndex((prevIndex) => (prevIndex + 1) % statsData.length);
-    }, 1000); // 슬라이드 전환 시간(이후 조절)
 
-    return () => clearInterval(interval);
-  }, [statsData.length]);
-  */
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setCurrentStatIndex((prevIndex) => (prevIndex + 1) % statsData.length);
+  //   }, 1000); // 슬라이드 전환 시간(이후 조절)
+  //
+  //   return () => clearInterval(interval);
+  // }, [statsData.length]);
+
   return (
-    <div className="flex border-[2px] border-[black] rounded-[12px] h-[300px] justify-normal items-center 
-                    ml-100 bg-[gray]">
+    <div className="flex border-[2px] border-[black] rounded-br-[40px] h-[300px] justify-normal items-center ml-100 bg-[black]  font-['NotoSansKR'] shadow-[0_25px_10px_-15px_rgba(0,0,0,0.3)]">
       {
         statsData.map((stat, index) => (
           <div
             key={index}
-            className={`text-[white] border-[2px] border-[white] rounded-[12px] h-[200px] mx-auto w-1/4`}>
-            <div className="h-full flex flex-col items-center justify-center text-center text-2xl">
+            className={`text-[white] border-[1px] border-[white] bg-zinc-900 border-solid rounded-[12px] h-[200px] mx-auto w-1/4 shadow-[20px_25px_10px_-15px_rgba(255,255,255,0.2)]`}>
+            <div className="h-full flex flex-col items-center justify-center text-center text-2xl font-bold">
               <div className="mb-[30px]">
                 {stat.label}</div>
               <div className="">
